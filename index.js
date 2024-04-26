bb610835a8c841b05b6ac4e0bce9e355fd845a8a
Anyelo <tprog.angel.salinas@outlook.com>
2024-04-26 10:10:15 -0600

    upgrade 1.1

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express(); // server 
+const PORT = 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.listen(3000, () => {
-    console.log('Server on port 3000');
+
+app.get('/Angel', (req, res) => {
+    const name = 'Angel'; // Reemplaza 'Tu nombre aquí' con tu nombre real
+    res.send(`Hello World! My name is ${name}`);
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor Express corriendo en http://localhost:${PORT}`);
 });
